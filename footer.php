e9fa01a9f9b990920ff79b7bd23ca879028f6d2f
trav <trvswgnr@gmail.com>
2019-03-03 21:15:52 -0500

    add footer

--- footer.php
+++ footer.php
@@ -6,13 +6,23 @@
  */
 
 ?>
-<section class="footer">
-<!-- some footer things -->
-</section>
+</div><!-- /.wrapper -->
+
+<footer>
+	<div class="social">
+		<a href="https://twitter.com" class="social__item social__icon"><i class="fa-fw fa fa-twitter"></i></a>
+		<a href="https://facebook.com" class="social__item social__icon"><i class="fa-fw fa fa-facebook"></i></a>
+		<a href="https://linkedin.com" class="social__item social__icon"><i class="fa-fw fa fa-linkedin"></i></a>
+		<a href="https://shareing.com" class="social__item">Share</a>
+	</div>
+	<div class="credit">
+		<span>© 2019 Unicorns Take Photos Too</span>
+	</div>
+</footer>
 
 <?php
 wp_footer();
 ?>
-	</div><!-- /#page -->
+</div><!-- /#page -->
 </body>
 </html>
