f3f1ffd90dd09f65e2bf19583ba2d5687fc7627f
trav <trvswgnr@gmail.com>
2019-03-03 21:17:25 -0500

    rename to home.php

--- templates/home.php
+++ templates/home.php
@@ -19,9 +19,9 @@
 			<?php foreach ( $gallery as $item ) : ?>
 				<a href="<?php echo esc_url( $item['link']['url'] ) ?: esc_url( $item['image']['url'] ); ?>" class="gallery__item" style="background-image: url(<?php echo esc_url( $item['image']['sizes']['medium'] ); ?>);"></a>
 			<?php endforeach; ?>
+				<a href="<?php echo esc_url( site_url() . '/portfolio' ); ?>" class="gallery__item gallery__view-more" ><span>View More</span></a>
 		</div>
 	<?php endif; ?>
-	</div>
 </main>
 
 <?php
