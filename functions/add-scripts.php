caa83937c386822b7ae1f77af985761bbaa2d5a2
trav <trvswgnr@gmail.com>
2019-03-05 14:59:56 -0500

    move main js to assets folder

--- functions/add-scripts.php
+++ functions/add-scripts.php
@@ -10,7 +10,7 @@ function bpm_scripts() {
 	wp_enqueue_script( 'bpm-skip-link-focus-fix', get_theme_file_uri( '/assets/js/skip-link-focus-fix.js' ), array(), '1.0', true );
 
 	// enqueue main script file.
-	$path = '/main.js';
+	$path = '/assets/js/main.js';
 	$ver  = filemtime( get_template_directory() . $path );
 	wp_enqueue_script( 'bpm-scripts', get_theme_file_uri( $path ), array( 'jquery' ), $ver, true );
 
