e2df22176d4e46ea788d2496fa0c0ee146ad61fe
trav <trvswgnr@gmail.com>
2019-03-06 02:36:55 -0500

    add gallery item title if it exists

--- templates/home.php
+++ templates/home.php
@@ -16,8 +16,15 @@
 	if ( $gallery ) :
 		?>
 		<div class="gallery gallery--home">
-			<?php foreach ( $gallery as $item ) : ?>
-				<a href="<?php echo esc_url( $item['link']['url'] ) ?: esc_url( $item['image']['url'] ); ?>" class="gallery__item" style="background-image: url(<?php echo esc_url( $item['image']['sizes']['large'] ); ?>);"></a>
+			<?php
+			foreach ( $gallery as $item ) :
+				$item_url = $item['link']['url'] ?: $item['image']['url'];
+				?>
+				<a href="<?php echo esc_url( $item_url ); ?>" class="gallery__item" style="background-image: url(<?php echo esc_url( $item['image']['sizes']['large'] ); ?>);">
+					<?php if ( $item['link']['title'] ) : ?>
+						<span class="gallery__title"><?php echo esc_html( $item['link']['title'] ); ?></span>
+					<?php endif; ?>
+				</a>
 			<?php endforeach; ?>
 				<a href="<?php echo esc_url( site_url() . '/portfolio' ); ?>" class="gallery__item gallery__view-more" ><span>View More</span></a>
 		</div>
